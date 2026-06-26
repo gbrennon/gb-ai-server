71c8deee8f87f28d592ca88db88c239d5b2f8ddf
Glauber Brennon <glauberbrennon@gmail.com>
2026-06-25 21:51:52 -0300

    refactor(compose): update imports to load components from new docker/podman directories

--- src/gb_ai_server/infrastructure/compose/__init__.py
+++ src/gb_ai_server/infrastructure/compose/__init__.py
@@ -1,17 +1,21 @@
 """Compose tool adapters (Docker Compose, Podman Compose)."""
 
-from .docker_builtin_runtime import DockerComposeBuiltin
-from .docker_builtin_lifecycle import DockerComposeBuiltinLifecycle
-from .docker_builtin_query import DockerComposeBuiltinQuery
-from .docker_standalone_runtime import DockerComposeStandalone
-from .docker_standalone_lifecycle import DockerComposeStandaloneLifecycle
-from .docker_standalone_query import DockerComposeStandaloneQuery
-from .podman_builtin_runtime import PodmanComposeBuiltin
-from .podman_builtin_lifecycle import PodmanComposeBuiltinLifecycle
-from .podman_builtin_query import PodmanComposeBuiltinQuery
-from .podman_standalone_runtime import PodmanComposeStandalone
-from .podman_standalone_lifecycle import PodmanComposeStandaloneLifecycle
-from .podman_standalone_query import PodmanComposeStandaloneQuery
+from ..docker import (
+    DockerComposeBuiltin,
+    DockerComposeBuiltinLifecycle,
+    DockerComposeBuiltinQuery,
+    DockerComposeStandalone,
+    DockerComposeStandaloneLifecycle,
+    DockerComposeStandaloneQuery,
+)
+from ..podman import (
+    PodmanComposeBuiltin,
+    PodmanComposeBuiltinLifecycle,
+    PodmanComposeBuiltinQuery,
+    PodmanComposeStandalone,
+    PodmanComposeStandaloneLifecycle,
+    PodmanComposeStandaloneQuery,
+)
 from .detector import FallbackComposeDetector
 
 __all__: list[str] = [
